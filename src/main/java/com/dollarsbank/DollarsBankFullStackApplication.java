e7022a1342bd1750918afd766be067b63e57f802
chriswoo59 <wooc99@gmail.com>
2023-04-06 13:36:55 -0400

    removed eureka client
    standalone upload, not going to upload the eureka server and api gateway

--- src/main/java/com/dollarsbank/DollarsBankFullStackApplication.java
+++ src/main/java/com/dollarsbank/DollarsBankFullStackApplication.java
@@ -2,10 +2,9 @@ package com.dollarsbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class DollarsBankFullStackApplication {
 
 	public static void main(String[] args) {
